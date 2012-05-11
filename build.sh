26753617aa982ecb8df27318318e64313cf43f40
Akira Maeda <glidenote@gmail.com>
2012-05-11 18:49:18 +0900

    typo修正とCentOS6用に修正

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 yum -y install rpm-build rpmdevtools protobuf-compiler protobuf-devel libutempter-devel zlib-devel ncurses-devel openssh-clients perl-IO-Tty
 rpmdev-setuptree
 cd ~/rpmbuild/SOURCES
-https://github.com/downloads/keithw/mosh/mosh-1.2.tar.gz
+wget https://github.com/downloads/keithw/mosh/mosh-1.2.tar.gz
 cd ~/rpmbuild/SPECS
 wget https://raw.github.com/glidenote/mosh-1.2-rpm/master/mosh12.spec
 rpmbuild -bb mosh12.spec
 ARCH=`uname -m`
+rpm -Uvh /root/rpmbuild/RPMS/${ARCH}/mosh-1.2-2.el6.${ARCH}.rpm
