1be7fe1d9bb56118962fa9fcaa6a701ce8c66927
Akira Maeda <glidenote@gmail.com>
2012-05-13 13:19:28 +0900

    yumとrpmコマンドだけsudoで実行するように修正

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yum -y install rpm-build rpmdevtools protobuf-compiler protobuf-devel libutempter-devel zlib-devel ncurses-devel openssh-clients perl-IO-Tty
+sudo yum -y install rpm-build rpmdevtools protobuf-compiler protobuf-devel libutempter-devel zlib-devel ncurses-devel openssh-clients perl-IO-Tty
 rpmdev-setuptree
 cd ~/rpmbuild/SOURCES
 wget https://github.com/downloads/keithw/mosh/mosh-1.2.tar.gz
@@ -8,4 +8,4 @@ cd ~/rpmbuild/SPECS
 wget https://raw.github.com/glidenote/mosh-1.2-rpm/master/mosh12.spec
 rpmbuild -bb mosh12.spec
 ARCH=`uname -m`
-rpm -Uvh /root/rpmbuild/RPMS/${ARCH}/mosh-1.2-2.el6.${ARCH}.rpm
+sudo rpm -Uvh ~/rpmbuild/RPMS/${ARCH}/mosh-1.2-2.el6.${ARCH}.rpm
