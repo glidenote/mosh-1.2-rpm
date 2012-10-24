6835b14d79e42a9e0db77e8bdd3d0073913c42f3
Akira Maeda <glidenote@gmail.com>
2012-10-24 23:22:04 +0900

    mosh-1.2.3に対応

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 sudo yum -y install rpm-build rpmdevtools protobuf-compiler protobuf-devel libutempter-devel zlib-devel ncurses-devel openssh-clients perl-IO-Tty
 rpmdev-setuptree
 cd ~/rpmbuild/SOURCES
-wget https://github.com/downloads/keithw/mosh/mosh-1.2.tar.gz
+wget https://github.com/downloads/keithw/mosh/mosh-1.2.3.tar.gz
 cd ~/rpmbuild/SPECS
-wget https://raw.github.com/glidenote/mosh-1.2-rpm/master/mosh12.spec
-rpmbuild -bb mosh12.spec
+wget https://raw.github.com/keithw/mosh/master/fedora/mosh.spec
+rpmbuild -bb mosh.spec
 ARCH=`uname -m`
-sudo rpm -Uvh ~/rpmbuild/RPMS/${ARCH}/mosh-1.2-2.el6.${ARCH}.rpm
+sudo rpm -Uvh ~/rpmbuild/RPMS/${ARCH}/mosh-1.2.3-1.el6.${ARCH}.rpm
