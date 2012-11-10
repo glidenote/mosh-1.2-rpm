c7580492982fc829f332fc370d79b99234eb32d4
Alice Kaerast <alice@kaerast.info>
2012-11-10 15:08:06 +0300

    add extra undocumented requirements

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo yum -y install rpm-build rpmdevtools protobuf-compiler protobuf-devel libutempter-devel zlib-devel ncurses-devel openssh-clients perl-IO-Tty
+sudo yum -y install rpm-build rpmdevtools protobuf-compiler protobuf-devel libutempter-devel zlib-devel ncurses-devel openssh-clients perl-IO-Tty openssl-devel gcc gcc-c++
 rpmdev-setuptree
 cd ~/rpmbuild/SOURCES
 wget https://github.com/downloads/keithw/mosh/mosh-1.2.3.tar.gz
