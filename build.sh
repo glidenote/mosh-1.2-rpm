b9f83c15cc3c2e592898e2c7fc2d7d09c034adb6
Akira Maeda <glidenote@gmail.com>
2013-04-24 11:06:52 +0900

    update mosh-1.2.4

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 sudo yum -y install rpm-build rpmdevtools protobuf-compiler protobuf-devel libutempter-devel zlib-devel ncurses-devel openssh-clients perl-IO-Tty openssl-devel gcc gcc-c++
 rpmdev-setuptree
 cd ~/rpmbuild/SOURCES
-wget https://github.com/downloads/keithw/mosh/mosh-1.2.3.tar.gz
+wget http://mosh.mit.edu/mosh-1.2.4.tar.gz
 cd ~/rpmbuild/SPECS
+rm mosh.spec*
 wget https://raw.github.com/keithw/mosh/master/fedora/mosh.spec
 rpmbuild -bb mosh.spec
 ARCH=`uname -m`
-sudo rpm -Uvh ~/rpmbuild/RPMS/${ARCH}/mosh-1.2.3-1.el6.${ARCH}.rpm
+sudo rpm -Uvh ~/rpmbuild/RPMS/${ARCH}/mosh-1.2.4-1.el6.${ARCH}.rpm
